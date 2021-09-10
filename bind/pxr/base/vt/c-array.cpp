7bd85ecaaf62f43dedcb9e46743eefce5babad85
Luke Titley <from+badja@luketitley.com>
2021-09-10 21:52:05 +0200

    Initial VtArray

---
+++ bind/pxr/base/vt/c-array.cpp
@@ -0,0 +1,26 @@
+#include <pxr/base/vt/value.h>
+#include <cppmm_bind.hpp>
+
+namespace cppmm_bind {
+
+namespace PXR_INTERNAL_NS {
+
+namespace pxr = ::PXR_INTERNAL_NS;
+
+template<typename ELEM>
+struct VtArray {
+    using BoundType = pxr::VtArray;
+
+    VtArray();
+
+    /// Destructor.
+    ~VtArray();
+
+} CPPMM_OPAQUEPTR CPPMM_IGNORE_UNBOUND; // struct VtArray
+
+template VtArray<bool>;
+
+} // namespace PXR_INTERNAL_NS
+
+} // namespace cppmm_bind
+
