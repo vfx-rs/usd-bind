da75baeb21ccd8d3416409259563a0fc16c640e2
Luke Titley <from+badja@luketitley.com>
2021-09-10 23:44:14 +0200

    Exposed the int types

--- bind/pxr/base/vt/c-array.cpp
+++ bind/pxr/base/vt/c-array.cpp
@@ -20,19 +20,30 @@ struct VtArray {
     size_t size() const;
     void push_back(ELEM const &elem);
 
-    /*
-    ElementType &operator[](size_t index);
-    ElementType const &operator[](size_t index) const;
-    */
+    ELEM &operator[](size_t index) CPPMM_RENAME(index_mut);
+    ELEM const &operator[](size_t index) const CPPMM_RENAME(index);
 
 } CPPMM_OPAQUEPTR CPPMM_IGNORE_UNBOUND; // struct VtArray
 
 template class VtArray<bool>;
+template class VtArray<uint8_t>;
+template class VtArray<int32_t>;
+template class VtArray<uint32_t>;
+template class VtArray<int64_t>;
+template class VtArray<uint64_t>;
 //template class VtArray<float>;
+//template class VtArray<double>;
 
 } // namespace PXR_INTERNAL_NS
 
 } // namespace cppmm_bind
 
 template class pxr::VtArray<bool>;
+template class pxr::VtArray<uint8_t>;
+template class pxr::VtArray<int32_t>;
+template class pxr::VtArray<uint32_t>;
+template class pxr::VtArray<int64_t>;
+template class pxr::VtArray<uint64_t>;
 //template class pxr::VtArray<float>;
+//template class pxr::VtArray<double>;
+
