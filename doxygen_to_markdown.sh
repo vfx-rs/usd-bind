49ada3145d5e56ac4e388f257bdbde5c28de9a58
Luke Titley <from+badja@luketitley.com>
2021-09-17 00:07:07 +0200

    Dont include bash files in comment reformatting

--- doxygen_to_markdown.sh
+++ doxygen_to_markdown.sh
@@ -1,12 +1,13 @@
 #!/bin/env bash
+files=`git ls-files -x '*.sh'`
 
 # Code blocks
-sed -i -e 's/\\code/```/g' `git ls-files`
-sed -i -e 's/\\endcode/```/g' `git ls-files`
+sed -i -e 's/\\code/```/g' $files
+sed -i -e 's/\\endcode/```/g' $files
 # List
-sed -i -e 's/\\li \([a-zA-Z0-9_]*\)/- \1/g' `git ls-files`
+sed -i -e 's/\\li \([a-zA-Z0-9_]*\)/- \1/g' $files
 # Italics
-sed -i -e 's/\\em \([a-zA-Z0-9_]*\)/*\1*/g' `git ls-files`
-sed -i -e 's/\\p \([a-zA-Z0-9_]*\)/*\1*/g' `git ls-files`
+sed -i -e 's/\\em \([a-zA-Z0-9_]*\)/*\1*/g' $files
+sed -i -e 's/\\p \([a-zA-Z0-9_]*\)/*\1*/g' $files
 # Bold
-sed -i -e 's/\\b \([a-zA-Z0-9_]*\)/**\1**/g' `git ls-files`
+sed -i -e 's/\\b \([a-zA-Z0-9_]*\)/**\1**/g' $files
