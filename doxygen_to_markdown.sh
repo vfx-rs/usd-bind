b6e226680c6453e5149a98684bcc1f7d510fb48e
Luke Titley <from+badja@luketitley.com>
2021-09-17 00:25:14 +0200

    Replace \sa with italics

--- doxygen_to_markdown.sh
+++ doxygen_to_markdown.sh
@@ -3,14 +3,13 @@ files=`git ls-files | grep -v doxygen`
 
 # Note
 sed -i -e 's/\\note /> /g' $files
-# Sa
-sed -i -e 's/\\sa /\n\/\/\/- /g' $files
 # Code blocks
 sed -i -e 's/\\code/```/g' $files
 sed -i -e 's/\\endcode/```/g' $files
 # List
 sed -i -e 's/\\li \([a-zA-Z0-9_]*\)/- \1/g' $files
 # Italics
+sed -i -e 's/\\sa \(.*\)/*\1*/g' $files
 sed -i -e 's/\\em \([a-zA-Z0-9_]*\)/*\1*/g' $files
 sed -i -e 's/\\p \([a-zA-Z0-9_]*\)/*\1*/g' $files
 sed -i -e 's/\\a \([a-zA-Z0-9_]*\)/*\1*/g' $files
