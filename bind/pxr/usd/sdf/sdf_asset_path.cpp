fb6d0994ccbae81ddcd99d6a0988078efd75e9c2
Anders Langlands <anderslanglands@gmail.com>
2021-09-29 12:46:24 +1300

    formatting

--- bind/pxr/usd/sdf/sdf_asset_path.cpp
+++ bind/pxr/usd/sdf/sdf_asset_path.cpp
@@ -1,5 +1,5 @@
-#include <pxr/usd/sdf/assetPath.h>
 #include <cppmm_bind.hpp>
+#include <pxr/usd/sdf/assetPath.h>
 
 namespace cppmm_bind {
 
@@ -8,7 +8,7 @@ namespace PXR_INTERNAL_NS {
 namespace pxr = ::PXR_INTERNAL_NS;
 
 /// \class SdfAssetPath
-/// 
+///
 /// Contains an asset path and an optional resolved path.
 struct SdfAssetPath {
     using BoundType = pxr::SdfAssetPath;
@@ -20,7 +20,8 @@ struct SdfAssetPath {
     SdfAssetPath(const std::string& path) CPPMM_RENAME(from_path);
 
     /// Construct an asset path with an associated resolved path.
-    SdfAssetPath(const std::string& path, const std::string& resolvedPath) CPPMM_RENAME(from_resolved_path);
+    SdfAssetPath(const std::string& path, const std::string& resolvedPath)
+        CPPMM_RENAME(from_resolved_path);
 
     /// Equality, including the resolved path.
     bool operator==(const pxr::SdfAssetPath& rhs) const;
@@ -35,26 +36,23 @@ struct SdfAssetPath {
     const std::string& GetAssetPath() const;
 
     /// Return the resolved asset path, if any.
-    /// 
+    ///
     /// Note that SdfAssetPath only carries a resolved path if the creator of
     /// an instance supplied one to the constructor.  SdfAssetPath will never
     /// perform any resolution itself.
     const std::string& GetResolvedPath() const;
 
-    SdfAssetPath(const pxr::SdfAssetPath& );
+    SdfAssetPath(const pxr::SdfAssetPath&);
 
-    SdfAssetPath(pxr::SdfAssetPath&& ) CPPMM_IGNORE;
+    SdfAssetPath(pxr::SdfAssetPath&&) CPPMM_IGNORE;
 
-    pxr::SdfAssetPath& operator=(const pxr::SdfAssetPath& );
+    pxr::SdfAssetPath& operator=(const pxr::SdfAssetPath&);
 
-    pxr::SdfAssetPath& operator=(pxr::SdfAssetPath&& ) CPPMM_IGNORE;
+    pxr::SdfAssetPath& operator=(pxr::SdfAssetPath&&) CPPMM_IGNORE;
 
     ~SdfAssetPath();
 
-
 } CPPMM_OPAQUEPTR; // struct SdfAssetPath
-
-
 
 } // namespace PXR_INTERNAL_NS
 
