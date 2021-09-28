38a4490f7e781b58abe29bb2e26576df259b0a87
Anders Langlands <anderslanglands@gmail.com>
2021-09-29 12:49:18 +1300

    prettier printing

--- usd-rs/examples/traverse_attributes/src/main.rs
+++ usd-rs/examples/traverse_attributes/src/main.rs
@@ -2,6 +2,9 @@ use std::path::Path;
 use usd::stage::{open, InitialLoadSet, UsdStage};
 use usd::time_code::UsdTimeCode;
 
+use std::io::{self, Write};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     let file = Path::new(manifest_dir)
@@ -10,23 +13,48 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         .parent()
         .unwrap()
         .join("data")
-        .join("box_vertex_colors.usda");
+        .join("twocubes.usda");
 
     let stage = open(&file, InitialLoadSet::All)?;
 
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
     for prim in stage.traverse() {
-        println!("{:?}: {}", prim, prim.type_name());
+        stdout
+            .set_color(
+                ColorSpec::new().set_bold(true).set_fg(Some(Color::White)),
+            )
+            .unwrap();
+        writeln!(&mut stdout, "{:?}: {}", prim, prim.type_name()).unwrap();
+        stdout
+            .set_color(
+                ColorSpec::new().set_bold(false).set_fg(Some(Color::White)),
+            )
+            .unwrap();
 
         for attr in &prim.attributes() {
-            print!(
-                "    {:<16} {:<32}",
-                attr.type_name().as_token(),
+            stdout
+                .set_color(
+                    ColorSpec::new().set_bold(false).set_fg(Some(Color::White)),
+                )
+                .unwrap();
+
+            writeln!(
+                &mut stdout,
+                "  {:16} {}",
+                attr.type_name().as_token().as_str(),
                 attr.name()
-            );
+            )
+            .unwrap();
             if let Some(v) = attr.get_value(UsdTimeCode::default()) {
-                println!(" = {}", v);
-            } else {
-                println!("");
+                stdout
+                    .set_color(
+                        ColorSpec::new()
+                            .set_bold(false)
+                            .set_fg(Some(Color::Blue)),
+                    )
+                    .unwrap();
+                writeln!(&mut stdout, "    {}", v).unwrap();
             }
         }
     }
