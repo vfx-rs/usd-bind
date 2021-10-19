05db15caeb51633bf9fcf325b65ad426b9b4404f
Luke Titley <from+badja@luketitley.com>
2021-10-19 14:09:39 +0200

    Brought online macro for building array types

--- usd-rs/crates/usd-vt/src/array_gfvec.rs
+++ usd-rs/crates/usd-vt/src/array_gfvec.rs
@@ -4,6 +4,79 @@ use std::ffi::CStr;
 use std::ops::Deref;
 use usd_sys as sys;
 
+#[macro_export]
+macro_rules! vt_vec_array {
+    ($oy:ty, $ty:ty, $elem:ident) => {
+paste::paste! {
+#[repr(transparent)]
+pub struct [<VtArray $elem>](pub(crate) *mut sys::[<pxr_VtArray $elem _t>]);
+
+unsafe impl OpaquePtr for [<VtArray $elem>] {
+    type SysPointee = sys::[<pxr_VtArray $elem _t>];
+    type Pointee = [<VtArray $elem>];
+}
+
+pub type [<VtArray $elem Ref>]<'a, P = [<VtArray $elem>]> = Ref<'a, P>;
+pub type [<VtArray $elem RefMut>]<'a, P = [<VtArray $elem>]> = RefMut<'a, P>;
+
+impl [<VtArray $elem>] {
+    pub fn new() -> Self {
+        let mut ptr = std::ptr::null_mut();
+        unsafe {
+            sys::[<pxr_VtArray $elem _ctor>](&mut ptr);
+        }
+        [<VtArray $elem>](ptr)
+    }
+
+    pub fn push<V: $oy>(&mut self, value: V) {
+        unsafe {
+            sys::[<pxr_VtArray $elem _push_back>](
+                self.0,
+                &value as *const _ as *const sys::[<pxr_ $elem _t>],
+            );
+        }
+    }
+
+    pub fn as_slice<V: $oy>(&self) -> &[V] {
+        let mut ptr = std::ptr::null();
+        unsafe {
+            sys::[<pxr_VtArray $elem _data_const>](self.0, &mut ptr);
+            std::slice::from_raw_parts(ptr as *const V, self.len())
+        }
+    }
+
+    pub fn reserve(&mut self, length: usize) {
+        unsafe {
+            sys::[<pxr_VtArray $elem _reserve>](self.0, length);
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        let mut result = 0_usize;
+        unsafe {
+            sys::[<pxr_VtArray $elem _size>](self.0, &mut result);
+            result
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+impl Drop for [<VtArray $elem>] {
+    fn drop(&mut self) {
+        unsafe {
+            sys::[<pxr_VtArray $elem _dtor>](self.0);
+        }
+        self.0 = std::ptr::null_mut();
+    }
+}
+}
+
+    };
+}
+
+vt_vec_array!(Vec2<f32>, f32, GfVec2f);
+
+/*
 //------------------------------------------------------------------------------
 // VtArrayGfVec2f
 //------------------------------------------------------------------------------
@@ -68,6 +141,7 @@ impl Drop for VtArrayGfVec2f {
         self.0 = std::ptr::null_mut();
     }
 }
+*/
 
 //------------------------------------------------------------------------------
 // VtArrayGfVec3f
