89e1584b419c76f9fac0ad061d6258e876b5aa98
Luke Titley <from+badja@luketitley.com>
2021-10-20 23:22:50 +0200

    Remove dead code

--- usd-rs/crates/usd-vt/src/value.rs
+++ usd-rs/crates/usd-vt/src/value.rs
@@ -765,40 +765,6 @@ impl ValueStore for [f16; 4] {
     }
 }
 
-/*
-impl ValueStore for [f32; 2] {
-    fn get(value: &VtValue) -> Option<&Self> {
-        let mut result: *const Self = std::ptr::null_mut();
-        unsafe {
-            sys::pxr_VtValue_Get_GfVec2f(
-                value.0,
-                &mut result as *mut *const _ as *mut *const sys::pxr_GfVec2f_t,
-            );
-            Some(&*result)
-        }
-    }
-
-    fn set(value: &mut VtValue, data: &Self) {
-        let mut dummy = std::ptr::null_mut();
-        unsafe {
-            sys::pxr_VtValue_assign_GfVec2f(
-                value.0,
-                &mut dummy,
-                *(data as *const _ as *const sys::pxr_GfVec2f_t),
-            );
-        }
-    }
-
-    fn is_holding(value: &VtValue) -> bool {
-        let mut result = false;
-        unsafe {
-            sys::value_is_holding_GfVec2f(&mut result, value.0);
-        }
-        result
-    }
-}
-*/
-
 macro_rules! slice_value_store {
     ($slice:tt, $elem:ident) => {
 paste::paste! {
