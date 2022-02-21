3174101b35d6f00395848643bd02e2d0cbe26543
Luke Titley <from+badja@luketitley.com>
2022-02-21 21:49:59 +0100

    Generate for sdf children

--- bind/pxr/usd/sdf/sdf_children.cpp
+++ bind/pxr/usd/sdf/sdf_children.cpp
@@ -1,4 +1,3 @@
-#if 0
 #include <pxr/usd/sdf/children.h>
 #include <cppmm_bind.hpp>
 
@@ -20,12 +19,16 @@ namespace pxr = ::PXR_INTERNAL_NS;
 template <class ChildPolicy>
 struct Sdf_Children {
     using BoundType = pxr::Sdf_Children<ChildPolicy>;
+    using ValueType = typename pxr::Sdf_Children<ChildPolicy>::ValueType;
+    using KeyType = typename pxr::Sdf_Children<ChildPolicy>::KeyType;
 
     Sdf_Children<ChildPolicy>();
 
-    Sdf_Children<ChildPolicy>(const pxr::Sdf_Children& other);
+    Sdf_Children(const pxr::Sdf_Children<ChildPolicy>& other);
 
-    Sdf_Children<ChildPolicy>(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& parentPath, const pxr::TfToken& childrenKey, const pxr::Sdf_Children::KeyPolicy& keyPolicy);
+#if 0
+    Sdf_Children(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& parentPath, const pxr::TfToken& childrenKey, const pxr::Sdf_Children<ChildPolicy>::KeyPolicy& keyPolicy);
+#endif
 
     /// Return whether this object is valid.
     bool IsValid() const;
@@ -34,26 +37,28 @@ struct Sdf_Children {
     size_t GetSize() const;
 
     /// Return the child at the specified index.
-    pxr::Sdf_Children::ValueType GetChild(size_t index) const;
+    ValueType GetChild(size_t index) const;
 
     /// Find the index of the specified key, or return the size if it's not found.
-    size_t Find(const pxr::Sdf_Children::KeyType& key) const;
+    size_t Find(const KeyType& key) const;
 
     /// Find the key that corresponds to \a value, or return a default
     /// constructed key if it's not found.
-    pxr::Sdf_Children::KeyType FindKey(const pxr::Sdf_Children::ValueType& value) const;
+    KeyType FindKey(const ValueType& value) const;
 
     /// Return true if this object and \a other are equivalent.
-    bool IsEqualTo(const pxr::Sdf_Children& other) const;
+    bool IsEqualTo(const pxr::Sdf_Children<ChildPolicy>& other) const;
 
     /// Replace this object's children with the ones in \a values.
+#if 0
     bool Copy(const UNKNOWN& values, const std::string& type);
+#endif
 
     /// Insert a new child at the specified \a index.
-    bool Insert(const pxr::Sdf_Children::ValueType& value, size_t index, const std::string& type);
+    bool Insert(const ValueType& value, size_t index, const std::string& type);
 
     /// Erase the child with the specified key.
-    bool Erase(const pxr::Sdf_Children::KeyType& key, const std::string& type);
+    bool Erase(const KeyType& key, const std::string& type);
 
 } CPPMM_OPAQUEPTR; // struct Sdf_Children
 
@@ -68,4 +73,3 @@ struct Sdf_Children {
 
 // TODO: fill in explicit instantiations
 // template class pxr::Sdf_Children<int>;
-#endif
