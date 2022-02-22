15d8b173b00f50b54238c97ef9db1f40720805d1
Luke Titley <from+badja@luketitley.com>
2022-02-22 22:34:08 +0100

    Added sdf_childrenn_utils

--- bind/pxr/usd/sdf/sdf_children_utils.cpp
+++ bind/pxr/usd/sdf/sdf_children_utils.cpp
@@ -1,9 +1,7 @@
-#if 0
 #include <pxr/usd/sdf/childrenUtils.h>
 #include <cppmm_bind.hpp>
 
 namespace cppmm_bind {
-
 namespace PXR_INTERNAL_NS {
 
 namespace pxr = ::PXR_INTERNAL_NS;
@@ -16,6 +14,7 @@ namespace pxr = ::PXR_INTERNAL_NS;
 template <class ChildPolicy>
 struct Sdf_ChildrenUtils {
     using BoundType = pxr::Sdf_ChildrenUtils<ChildPolicy>;
+    using FieldType = typename pxr::Sdf_ChildrenUtils<ChildPolicy>::FieldType;
 
     /// Create a new spec in \a layer at \childPath and add it to its parent's
     /// field named \childrenKey. Emit an error and return false if the new spec
@@ -25,22 +24,23 @@ struct Sdf_ChildrenUtils {
     static bool CreateSpec(pxr::SdfLayer* layer, const pxr::SdfPath& childPath, pxr::SdfSpecType specType, bool inert);
 
     /// Return whether \a newName is a valid name for a child.
-    static bool IsValidName(const pxr::Sdf_ChildrenUtils::FieldType& newName);
+    static bool IsValidName(const FieldType& newName);
 
     /// Return whether \a newName is a valid name for a child.
     static bool IsValidName(const std::string& newName);
 
     /// Return whether \a spec can be renamed to \a newName.
-    static pxr::SdfAllowed CanRename(const pxr::SdfSpec& spec, const pxr::Sdf_ChildrenUtils::FieldType& newName);
+    static pxr::SdfAllowed CanRename(const pxr::SdfSpec& spec, const FieldType& newName);
 
     /// Rename \a spec to \a newName. If \a fixPrimListEdits is true,
     /// then also fix up the name children order. It's an error for
     /// \a fixPrimListEdits to be true if spec is not a PrimSpec.
-    static bool Rename(const pxr::SdfSpec& spec, const pxr::Sdf_ChildrenUtils::FieldType& newName);
+    static bool Rename(const pxr::SdfSpec& spec, const FieldType& newName);
 
     /// Replace the children of the spec at \a path with the specs in \a
     /// values. This will delete existing children that aren't in \a values and
     /// reparent children from other locations in the layer.
+#if 0
     static bool SetChildren(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const UNKNOWN& values);
 
     /// Insert \a value as a child of \a path at the specified index.
@@ -51,7 +51,7 @@ struct Sdf_ChildrenUtils {
 
     /// Insert \a value as a child of \a path at the specified index with
     /// the new name \p newName.
-    static bool MoveChildForBatchNamespaceEdit(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const UNKNOWN& value, const pxr::Sdf_ChildrenUtils::FieldType& newName, int index);
+    static bool MoveChildForBatchNamespaceEdit(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const UNKNOWN& value, const FieldType& newName, int index);
 
     /// Remove the child identified by \a key.
     static bool RemoveChildForBatchNamespaceEdit(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const UNKNOWN& key);
@@ -59,11 +59,12 @@ struct Sdf_ChildrenUtils {
     /// Returns \c true if \p value can be inserted as a child of \p path
     /// with the new name \p newName at the index \p index, otherwise
     /// returns \c false and sets \p whyNot.
-    static bool CanMoveChildForBatchNamespaceEdit(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const UNKNOWN& value, const pxr::Sdf_ChildrenUtils::FieldType& newName, int index, std::string* whyNot);
+    static bool CanMoveChildForBatchNamespaceEdit(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const UNKNOWN& value, const FieldType& newName, int index, std::string* whyNot);
+#endif
 
     /// Returns \c true if the child of \p path identified by \p key can
     /// be removed, otherwise returns \c false and sets \p whyNot.
-    static bool CanRemoveChildForBatchNamespaceEdit(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const pxr::Sdf_ChildrenUtils::FieldType& key, std::string* whyNot);
+    static bool CanRemoveChildForBatchNamespaceEdit(const pxr::SdfLayerHandle& layer, const pxr::SdfPath& path, const FieldType& key, std::string* whyNot);
 
 } CPPMM_OPAQUEPTR; // struct Sdf_ChildrenUtils
 
@@ -78,4 +79,3 @@ struct Sdf_ChildrenUtils {
 
 // TODO: fill in explicit instantiations
 // template class pxr::Sdf_ChildrenUtils<int>;
-#endif
